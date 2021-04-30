0f0f2bc543973292d46bbc3aee9caf0d763b17e3
Uncle-Ma11 <1933511939@qq.com>
2021-04-29 22:24:19 -0400

    add a method

--- src/main.java
+++ src/main.java
@@ -1,4 +1,9 @@
 public class main {
+    
+    public static void calculate(int[] a){
+
+    }
+
     public static void main(String[]args){
         System.out.println("This is my project.");
     }
